e8e80bff0ddf80de3860094c3639c95bd48fb25e
simboltwo <brun4j0g0s@gmail.com>
2025-11-09 12:56:04 -0300

    commit #23

--- naapi-feature-crud-diagnosticos/src/main/resources/import.sql
+++ naapi-feature-crud-diagnosticos/src/main/resources/import.sql
@@ -6,27 +6,34 @@ INSERT INTO TbPapel (DsAuthority) VALUES ('ROLE_COORDENADOR_CURSO'); -- ID 4
 INSERT INTO TbPapel (DsAuthority) VALUES ('ROLE_PROFESSOR');         -- ID 5
 
 -- 2. Inserir Usuários
--- A senha para ambos é "123456" (encriptada com BCrypt)
+-- A senha para todos é "123456" (encriptada com BCrypt)
 INSERT INTO TbUsuario (NmUsuario, DsEmail, DsSenha) VALUES ('Admin Coordenador', 'coordenador@naapi.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3e3taqL.zU5aTxtEnA3PAewQYhQOP3y'); -- ID 1
 INSERT INTO TbUsuario (NmUsuario, DsEmail, DsSenha) VALUES ('Membro Tecnico', 'membro@naapi.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3e3taqL.zU5aTxtEnA3PAewQYhQOP3y');    -- ID 2
+INSERT INTO TbUsuario (NmUsuario, DsEmail, DsSenha) VALUES ('Estagiario 1 (Assistente)', 'estagiario@naapi.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3e3taqL.zU5aTxtEnA3PAewQYhQOP3y'); -- ID 3
 
 -- 3. Ligar Usuários aos Papéis
 INSERT INTO TbUsuarioPapel (CdUsuario, CdPapel) VALUES (1, 1); -- Admin é Coordenador NAAPI
 INSERT INTO TbUsuarioPapel (CdUsuario, CdPapel) VALUES (2, 2); -- Membro é Membro Técnico
+INSERT INTO TbUsuarioPapel (CdUsuario, CdPapel) VALUES (3, 3); -- Estagiario é Estagiário (Assistente)
 
--- 4. Inserir Dados para Alunos (para os testes de DELETE e GET)
+-- 4. Inserir Dados para Alunos (Cursos, Turmas, Diagnósticos)
 INSERT INTO TbCurso (NmCurso) VALUES ('Engenharia de Computação'); -- ID 1
 INSERT INTO TbCurso (NmCurso) VALUES ('Arquitetura'); -- ID 2
 
-INSERT INTO TbTurma (NmTurma) VALUES ('INFO-2023'); -- ID 1
+INSERT INTO TbTurma (NmTurma) VALUES ('COM-2023'); -- ID 1
 INSERT INTO TbTurma (NmTurma) VALUES ('ARQ-2022'); -- ID 2
 
 INSERT INTO TbDiagnostico (cid, NmDiagnostico, SIGLA) VALUES ('F84.0', 'Autismo', 'TEA'); -- ID 1
+INSERT INTO TbDiagnostico (cid, NmDiagnostico, SIGLA) VALUES ('F90.0', 'Hiperatividade', 'TDAH'); -- ID 2
 
--- Inserir Alunos (ic_ativo = true por defeito, como definido na entidade)
-INSERT INTO TbAluno (NmAluno, CdMatricula, IcPrioridade, CdCurso, CdTurma) VALUES ('Maria Silva', '123456', false, 1, 1); -- ID 1
-INSERT INTO TbAluno (NmAluno, NmSocial, CdMatricula, IcPrioridade, CdCurso, CdTurma) VALUES ('João Souza', 'Joana', '789012', true, 2, 2); -- ID 2
-INSERT INTO TbAluno (NmAluno, CdMatricula, IcPrioridade, CdCurso, CdTurma) VALUES ('Carlos Pereira', '654321', false, 1, 1); -- ID 3
+-- 5. INSERIR TIPOS DE ATENDIMENTO (O QUE ESTAVA FALTANDO)
+INSERT INTO TbTipoAtendimento (NmAtendimento) VALUES ('Assistência');     -- ID 1
+INSERT INTO TbTipoAtendimento (NmAtendimento) VALUES ('Orientação de Estudos'); -- ID 3
+
+-- 6. Inserir Alunos (Base para testes)
+INSERT INTO TbAluno (NmAluno, CdMatricula, DsPrioridade, CdCurso, CdTurma) VALUES ('Maria Silva', '123456', 'Baixa', 1, 1); -- ID 1
+INSERT INTO TbAluno (NmAluno, NmSocial, CdMatricula, DsPrioridade, CdCurso, CdTurma) VALUES ('João Souza', 'Joana', '789012', 'Alta', 2, 2); -- ID 2
+INSERT INTO TbAluno (NmAluno, CdMatricula, DsPrioridade, CdCurso, CdTurma) VALUES ('Carlos Pereira', '654321', 'Média', 1, 1); -- ID 3
 
 -- Ligar Aluno 1 ao Diagnóstico 1
 INSERT INTO TbAlunoDiagnostico (CdAluno, CdDiagnostico) VALUES (1, 1);
